7bec09b8ed5fe887799e7a9ec79696601036b389
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-07-27 11:03:32 +0100

    TEST

--- post-pr-malformed.py
+++ post-pr-malformed.py
@@ -1,5 +1,6 @@
 def func_x(num):        
     if num == 1:
         return a()
-    elif num == 2:
+    elif num == 2:          
         return b()
+    
--- pre-pr-malformed.tf
+++ pre-pr-malformed.tf
@@ -1,3 +1,4 @@
-module "test" {
+module "test" {     
   repository = "acronyms"
-}
+}   
+  
